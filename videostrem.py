4202cd1239d925991bd6982bafe556ee681833d7
JUANROB-MOTOR <juanduran0227@gmail.com>
2024-03-11 08:53:16 -0500

    cambios finales

--- videostrem.py
+++ videostrem.py
@@ -1,11 +1,7 @@
 from flask import Flask, render_template, Response
-from flask import Flask, render_template, Response
 import cv2
-import requests
 app=Flask(__name__)
 camera = cv2.VideoCapture(0)
-
-# URL a la que enviar el video
 
 
 def gen_frames():  
@@ -29,20 +25,14 @@ def gen_frames():
 
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
-            # Enviar el frame a la URL especificada
-            
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
-  
-    numero = 42
-    return render_template('index.html', numero=numero)
-    
+    return render_template('index.html')
 @app.route('/video_feed')
 def video_feed():
-    
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 @app.route('/ejecutar_codigo', methods=['POST'])
 def ejecutar_codigo():
@@ -50,5 +40,6 @@ def ejecutar_codigo():
     print("Código de Python ejecutado")
     return "Código de Python ejecutado con éxito"
 if __name__=='__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', port=5000, debug=True)
+
 
